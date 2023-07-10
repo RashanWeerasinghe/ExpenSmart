7147ac5558e9a1a3c9bbdd23e3990e5c3b6c99f9
RashanWeerasinghe <malithrashan03@gmail.com>
2023-07-11 01:37:06 +0530

    first commit

---
+++ ExpenSmart/src/test/java/com/example/ExpenSmart/ExpenSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpenSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
